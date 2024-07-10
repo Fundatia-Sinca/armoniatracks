8c84de1c5cf812e896845965e188c830f7c309f9
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-07-10 13:44:40 +0300

    The right hours for the form

--- frontend/src/components/Form.vue
+++ frontend/src/components/Form.vue
@@ -93,6 +93,8 @@
       </label>
       <div class="relative">
         <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-date-hour">
+          <option>00:00</option>
+          <option>00:30</option>
           <option>01:00</option>
           <option>01:30</option>
           <option>02:00</option>
@@ -139,8 +141,6 @@
           <option>22:30</option>
           <option>23:00</option>
           <option>23:30</option>
-          <option>24:00</option>
-          <option>24:30</option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
