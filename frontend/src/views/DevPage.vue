fc7f01aa1e4cc495464ffd09ab862d402eb00618
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-07-12 12:34:54 +0300

    the way that the form looks

--- frontend/src/views/DevPage.vue
+++ frontend/src/views/DevPage.vue
@@ -2,14 +2,10 @@
 import Form from "@/components/Form.vue" 
 import Navbar from "@/components/Navbar.vue" </script>
 <template>
-    <div class="flex flex-col gap-y-5 p-5">
-        <span class="text-4xl text-black">Development</span>
-        <RouterLink :to="{ name: 'homepage' }" class="text-md text-red-500 shadow-xl uppercase" aria-current="page">Back
-            to Homepage</RouterLink>
+    <div class="flex flex-col gap-y-5 p-5 mt-20 bg-yellow-200">
+        <span class="text-4xl text-black">Schedule</span>
     </div>
 
-
-    <span class="p-5">Aici lucrati.</span>
     <Navbar/>
     <Form/>
 
