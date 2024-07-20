0736bf70cc8dcc81d00b2ceacb044645653eee77
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-07-20 11:31:55 +0300

    signin added

--- frontend/src/components/NavBarCP.vue
+++ frontend/src/components/NavBarCP.vue
@@ -37,10 +37,10 @@
         class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
         Contact</RouterLink>
       <div class="p-2 bg-yellow-200 rounded-lg hover:bg-blue-700 flex items-center justify-center">
-        <button class="text-2xl text-black hover:text-white" @click="notify">🔔</button>
+        <RouterLink :to="{ name: 'homepage' }" class="text-2xl text-black hover:text-white">🔔</RouterLink> 
       </div>
       <div class="p-2 bg-yellow-200 rounded-lg hover:bg-blue-700 flex items-center justify-center">
-        <button class="text-2xl text-black hover:text-white" @click="signIn">👤</button>
+        <RouterLink :to="{ name: 'signinpage' }" class="text-2xl text-black hover:text-white">👤</RouterLink>
       </div>
     </div>
   </div>
